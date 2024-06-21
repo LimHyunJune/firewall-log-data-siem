60d39d41f1575c217390526f3e1460d0c25a0d7e
Imu <48524793+LimHyunJune@users.noreply.github.com>
2024-06-21 15:30:18 +0900

    feat: swagger 연동

---
+++ firewall-log-data-siem/src/main/java/org/example/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package org.example.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("API Controller") // API의 제목
+                .description("Firewall Data Collection") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
+    }
+}
--- firewall-log-data-siem/src/main/java/org/example/controller/APIController.java
+++ firewall-log-data-siem/src/main/java/org/example/controller/APIController.java
@@ -1,11 +1,15 @@
 package org.example.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "API Controller")
 public class APIController {
 
+    @Operation(summary = "test api")
     @GetMapping("/test")
     public String test()
     {
